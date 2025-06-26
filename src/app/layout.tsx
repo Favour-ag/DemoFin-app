81332c4a865bc5a7742b678460d219bb44f31ad2
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2025-06-26 16:25:54 +0100

    fixed the sidepanel layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,37 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import SidePanel from "@/components/SidePanel";
+// import Header from "@/components/Header";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Spendin Admin",
+//   description: "Spendin Admin",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en" suppressHydrationWarning>
+//       <body className={`${inter.className} `}>
+//         <div className="w-full flex max-w-[1800px] mx-auto justify-center font-inter bg-gray-50 ">
+//           <div className="w-1/6 max-w-1/6  ">
+//             <SidePanel />
+//           </div>
+//           <div className=" w-5/6 max-w-5/6  ">
+//             <Header />
+//             {children}
+//           </div>
+//         </div>
+//       </body>
+//     </html>
+//   );
+// }
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,15 +52,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} `}>
-        <div className="w-full flex max-w-[1800px] mx-auto justify-center font-inter bg-gray-50 ">
-          <div className="w-1/6 max-w-1/6  ">
-            <SidePanel />
-          </div>
-          <div className=" w-5/6 max-w-5/6  ">
-            <Header />
-            {children}
-          </div>
+      <body className={`${inter.className}`}>
+        <SidePanel />
+
+        <div className="ml-20 lg:ml-56 min-h-screen bg-gray-50 font-inter">
+          <Header />
+          {children}
         </div>
       </body>
     </html>
